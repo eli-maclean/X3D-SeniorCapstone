aaafbfd173ea56afbae9932f2dcfee47e7b03607
eli-maclean <em07614@georgiasouthern.edu>
2020-11-17 22:25:24 -0500

    changed home button pointers to test_repo

--- modelpages/basic2.php
+++ modelpages/basic2.php
@@ -37,7 +37,7 @@ $db_handle = new DBController();
                                         <table class=wrt>
                                             <tr>
 													<td class=wrf><a class=wrf title="" href=index.html>Previous</a></td>
-                                                    <td class=wrf><a class=wrf title="" href="http://web-students.armstrong.edu/~ja17173/senior/3dmodels.php"> Home </a></td>
+                                                    <td class=wrf><a class=wrf title="" href="http://http://web-students.armstrong.edu/~em07614/test_repo/active/3dmodels.php"> Home </a></td>
                                                     <td class=wrf> <div class="dropdown"> <a class=wrt href=../basic.php> Basic </a> 
                                                                                 <div class="dropdown-content">
                                                                                 <?php $product_array = $db_handle->runQuery("SELECT * FROM models where type = 'basic'");
