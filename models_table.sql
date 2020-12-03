6bad87a00676bf5c1ecb30c90ba18ecb45a09e23
eli-maclean <em07614@georgiasouthern.edu>
2020-12-03 01:49:29 -0500

    added sources table and changed "id" to "modelid" in models table

--- models_table.sql
+++ models_table.sql
@@ -1,17 +1,19 @@
 --create table statement
 CREATE TABLE `models` (
-  `id` int(10) NOT NULL DEFAULT '0',
+  `modelid` int(10) NOT NULL DEFAULT '0',
   `name` varchar(50) DEFAULT NULL,
   `thumbnail` varchar(100) DEFAULT NULL,
   `adr` varchar(100) DEFAULT NULL,
   `x3d-loc` varchar(100) DEFAULT NULL,
   `type` varchar(12) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `disabled` boolean NOT NULL DEFAULT false,
+  PRIMARY KEY (`modelid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 
 --dump table data
-INSERT INTO `models` (`id`,`name`,`thumbnail`,`adr`,`x3d-loc`,`type`) VALUES
+--changes to id numbers on this page must be applied to sources_table.sql as well 
+INSERT INTO `models` (`modelid`,`name`,`thumbnail`,`adr`,`x3d-loc`,`type`) VALUES
  (0,"Brain","thumbnails/brain.png","modelpages/brain.php","x3d/Brain/Brain_nocompression.x3d","decomposable"),
  (1,"Skull","thumbnails/skull.png","modelpages/skull.php","x3d/Skull/skullAssembly_noscript.x3d","decomposable"),
  (2,"Body Skin","thumbnails/bodyskin.png","","x3d/BodySkin/BodySkinIndexedFaceSetNIST.x3d","basic"),
@@ -36,8 +38,16 @@ INSERT INTO `models` (`id`,`name`,`thumbnail`,`adr`,`x3d-loc`,`type`) VALUES
  (21,"Maxillary","thumbnails/Maxillary.png","","x3d/Maxillary/Maxillary.x3d","basic"),
  (22,"Heart and Kidney","thumbnails/heartkidney.png","","x3d/Heartkidney/HeartKidney.x3d","basic"),
  (23,"Mandibular","thumbnails/mandibular.png","","x3d/Mandibular/Mandibular.x3d","basic"),
- (24,"Respiratory System","thumbnails/respiratorysystem.png","","x3d/RespiratorySystem/RespiratorySystem.x3d","basic"),
- (25,"Volumetric Abdomen","thumbnails/abdomenvol.png","","x3d/abdomen_volume/BasicAbdomen.x3d","volume"),
- (26,"Volumetric Body","thumbnails/bodyvol.png","","x3d/body_volume/BasicBody.x3d","volume"),
- (27,"Volumetric Brain","thumbnails/brainvol.png","","x3d/brain_volume/BasicBrain.x3d","volume"),
- (28,"Volumetric Skull","thumbnails/skullvol.png","","x3d/skull_volume/BasicSkull.x3d","volume");
+ (24,"Mandible","thumbnails/mandable.png","","x3d/Mandible/BonesMandible.x3d","basic"),
+ (25,"Spine","thumbnails/spine.png","","x3d/Spine/BonesSpine.x3d","basic"),
+ (26,"Aorta Aneurism","thumbnails/aortaaneurism.png","","x3d/Aneurism of Ascending Aort/Ascending_Aort_Aneurism_Nevit_Dilmen.x3d","basic"),
+ (27,"Covid 19","thumbnails/covid19.png","","x3d/Covid 19/Covid 19.x3d","basic"),
+ (28,"Horseshoe Kidney","thumbnails/horseshoekidney.png","","x3d/Horseshoe Kidney/Horseshoe Kidney.x3d","basic"),
+ (29,"Pulmonary Atresia","thumbnails/pulmonaryatresia.png","","x3d/Pulmonary atresia with ventricular septal defect/CM0024_BP_0.x3d","basic"),
+ (30,"Kypho-Scoliosis","thumbnails/kypho-scoliosis.png","","x3d/Severe congenital kypho-scoliosis/V2_Spinal Lumbar_T10_L2.x3d","basic"),
+ (31,"Skull and Neck","thumbnails/skullandneck.png","","x3d/Skull and Neck/ALM0019.x3d","basic"),
+ (32,"Respiratory System","thumbnails/respiratorysystem.png","","x3d/RespiratorySystem/RespiratorySystem.x3d","basic"),
+ (33,"Volumetric Abdomen","thumbnails/abdomenvol.png","","x3d/abdomen_volume/BasicAbdomen.x3d","volume"),
+ (34,"Volumetric Body","thumbnails/bodyvol.png","","x3d/body_volume/BasicBody.x3d","volume"),
+ (35,"Volumetric Brain","thumbnails/brainvol.png","","x3d/brain_volume/BasicBrain.x3d","volume"),
+ (36,"Volumetric Skull","thumbnails/skullvol.png","","x3d/skull_volume/BasicSkull.x3d","volume");
